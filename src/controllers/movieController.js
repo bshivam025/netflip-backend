111245ba48028188a40d574669c0334382a471a7
Shivam Bhardwaj <bshiam472@gmail.com>
2025-04-19 19:57:05 +0530

    more routes

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -52,4 +52,88 @@ async function getTopRated (req, res, next) {
     }
 }
 
-export { getNowPlaying, getPopular, getTopRated };
+async function getMovieById(req, res, next) {
+    let movieId = req.params.id;
+    try{
+        if(movieId){
+            let api = TMDB_APIS.get_movie_by_id + movieId + "?";
+            let result = await fetch(api, getApiOptionsTmdb());
+            result = await result.json();
+            res.json({
+                success: true,
+                data: result
+            });
+        }
+    } catch(error){
+        console.error("error in fetching now playing movies : ", error);
+        res.json({
+            success: false,
+            data: "Something went wrong"
+        })
+    }
+}
+
+async function getMovieVideo(req, res, next) {
+    let movieId = req.params.id;
+    try{
+        if(movieId){
+            let api = TMDB_APIS.get_movie_video.replace('{MOVIE_ID}', movieId);
+            let result = await fetch(api, getApiOptionsTmdb());
+            result = await result.json();
+            res.json({
+                success: true,
+                data: result
+            });
+        }
+    } catch(error){
+        console.error("error in fetching now playing movies : ", error);
+        res.json({
+            success: false,
+            data: "Something went wrong"
+        })
+    }
+}
+
+async function getSimilarMovie(req, res, next) {
+    let movieId = req.params.id;
+    try{
+        if(movieId){
+            let api = TMDB_APIS.get_similar_movies.replace('{MOVIE_ID}', movieId);
+            let result = await fetch(api, getApiOptionsTmdb());
+            result = await result.json();
+            res.json({
+                success: true,
+                data: result
+            });
+        }
+    } catch(error){
+        console.error("error in fetching now playing movies : ", error);
+        res.json({
+            success: false,
+            data: "Something went wrong"
+        })
+    }
+}
+
+async function getMovieBySearch(req, res, next) {
+    let movieName = req.params.name;
+    try{
+        if(movieName){
+            let api = TMDB_APIS.get_search_result + movieName;
+            let result = await fetch(api, getApiOptionsTmdb());
+            result = await result.json();
+            res.json({
+                success: true,
+                data: result
+            });
+        }
+    } catch(error){
+        console.error("error in fetching now playing movies : ", error);
+        res.json({
+            success: false,
+            data: "Something went wrong"
+        })
+    }
+}
+
+export { getNowPlaying, getPopular, getTopRated, getMovieById, getMovieBySearch, getMovieVideo, getSimilarMovie};
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { getNowPlaying, getPopular, getTopRated } from "../controllers/movieController.js";
+import { getNowPlaying, getPopular, getTopRated, getMovieById, getMovieBySearch, getMovieVideo, getSimilarMovie } from "../controllers/movieController.js";
 
 router.get("/now-playing", getNowPlaying);
 router.get("/popular", getPopular);
 router.get("/top-rated", getTopRated);
+router.get("/fetch-movie-by-id/:id", getMovieById);
+router.get("/fetch-movie-by-search/:name", getMovieBySearch);
+router.get("/fetch-similar-movie/:id", getSimilarMovie);
 
 export default router;
